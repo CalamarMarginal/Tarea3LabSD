170bf5d95b27ff1b7216941d44de9ac78e278daf
Nicole Gallardo <nicole.gallardo@sansano.usm.cl>
2020-12-29 17:55:55 -0300

    DNS que responden a broker

--- tests/DNS/dns1.go
+++ tests/DNS/dns1.go
@@ -8,16 +8,22 @@ import (
 	"sync"
 
 	adminDNSpb "./adminDNSpb"
+	brokerDNSpb "./brokerDNSpb"
 
 	"google.golang.org/grpc"
 )
 
 const ipBroker string = "0.0.0.0:50058"
-const ipDNS1 string = "0.0.0.0:50052" //ip propia
+const ipDNS1 string = "0.0.0.0:50052" //puerto propio
 const ipDNS2 string = "0.0.0.0:50053"
 const ipDNS3 string = "0.0.0.0:50054"
 
+const ipDNS1Broker string = "0.0.0.0:50055" //puerto propio
+const ipDNS2Broker string = "0.0.0.0:50056"
+const ipDNS3Broker string = "0.0.0.0:50057"
+
 type serverAdmin struct{}
+type serverBroker struct{}
 
 /*
 func ping(ip string) bool {
@@ -38,6 +44,15 @@ func (*serverAdmin) AdminDNSComm(ctx context.Context, req *adminDNSpb.CommandAdm
 	ack := "escuche tu comando"
 	res := &adminDNSpb.DnsResponse{
 		Ack: ack,
+	}
+	return res, nil
+}
+
+func (*serverBroker) BrokerDNSComm(ctx context.Context, req *brokerDNSpb.ClienteBrRequest) (*brokerDNSpb.DnsClientResponse, error) {
+	fmt.Println("Request recibido:", req.CommCliente)
+	ack := "tu pagina esta en 10.11.12.13"
+	res := &brokerDNSpb.DnsClientResponse{
+		IpDominio: ack,
 	}
 	return res, nil
 }
@@ -63,14 +78,35 @@ func ServerA() { //servidor para admin
 	}
 }
 
+func ServerB() { //servidor para broker
+	fmt.Println("DNS broker server is running")
+
+	lis, err := net.Listen("tcp", ipDNS1Broker) //este puerto usa el broker para conectarse
+
+	if err != nil {
+		log.Fatalf("Failed to listen %v", err)
+	}
+
+	//asignar servidor de grpc a s
+	s := grpc.NewServer()
+
+	//se utiliza el archivo que se genera por el protocol buffer y utilizaremos el metodo Register y el nombre del servicio
+	// le pasasomos el servidor de grpc (s) y la estructura de un servidor "server"
+	brokerDNSpb.RegisterBrokerDNSServiceServer(s, &serverBroker{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve %v", err)
+	}
+}
+
 func main() {
 
 	var wg sync.WaitGroup
 	wg.Add(4)
 
-	//server de admin y server de cliente
+	//server de admin y server de broker
 	go ServerA()
-	//go ClientServer()
+	go ServerB()
 	wg.Wait()
 	return
 }
